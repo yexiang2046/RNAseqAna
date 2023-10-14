d3ba3e42bcf247c4ee464c4020180ba7f3824ebe
Xiang Ye <yexiang2046@gmail.com>
2023-10-13 21:57:17 -0500

    Add helper message to fastqc.sh

--- bash/fastqc.sh
+++ bash/fastqc.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
-
+# add fastqc to path
 export PATH=$PATH:/home/xiang/src/FastQC
 
+# help info
+if [ $# -lt 3 ]; then
+    echo "Usage: $0 <fastqc_out_dir> <threads> <fastq_file>"
+    echo "Example: $0 fastqc_out 4 sample.fastq"
+    echo "Note: fastqc_out_dir will be created if not exists"
+    exit 1
+fi
+
+# parameters
 FASTQC_OUT=$1
 THREADS=$2
 FASTQ_FILE=$3
