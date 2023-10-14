99da4ce6c23547fd13db4dead82579e98d5a5e7f
Xiang Ye <yexiang2046@gmail.com>
2023-10-13 22:26:53 -0500

    Add installation info for gffread

--- bash/gff3_to_gtf.sh
+++ bash/gff3_to_gtf.sh
@@ -1,1 +1,7 @@
+# https://github.com/gpertea/gffread
+# installation
+# cd /some/build/dir
+# git clone https://github.com/gpertea/gffread
+# cd gffread
+# make release
 gffread GRCh38_HHV8.annotation.gff3 -g ~/Dropbox/GRCh38.primary_GQ994935.fa -o GRCh38_HHV8.annotation.gtf
