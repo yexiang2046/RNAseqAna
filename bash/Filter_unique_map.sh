f22708b6db26c0a41d4cbb6a1eba7018d8d83eee
Xiang Ye <yexiang2046@gmail.com>
2023-11-26 11:25:33 -0600

    Add chromosome filter for mouse and human or custom chromsomes

--- bash/Filter_unique_map.sh
+++ bash/Filter_unique_map.sh
@@ -6,7 +6,22 @@ set -Eeuo pipefail
 # folder for fastq files
 FILE_PATH=$1
 ALIGNMENT_PATH=$2
+# can be mouse, human or custom chromsomes provided
+CHROMOSOMES=$3
 
+
+# mouse chromosomes
+MOUSE_CHROMOSOM="chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chrX chrY"
+HUMAN_CHROMOSOM="chr1 chr2 chr3 chr4 chr5 chr6 chr7 chr8 chr9 chr10 chr11 chr12 chr13 chr14 chr15 chr16 chr17 chr18 chr19 chr20 chr21 chr22 chrX chrY"
+
+# assign CHROMSOMES
+if [ ${CHROMOSOMES} == "mouse" ]; then
+    CHROMOSOMES_KEPT=${MOUSE_CHROMOSOM}
+elif [ ${CHROMOSOMES} == "human" ]; then
+    CHROMOSOMES_KEPT=${HUMAN_CHROMOSOM}
+else 
+    CHROMOSOMES_KEPT=${CHROMOSOMES}
+fi
 
 
 
@@ -15,5 +30,7 @@ samples_id=$(ls ${FILE_PATH} | awk -v FS='\t' -v ORS='\n' 'match($0, /^.*_001.fa
 
 for f in ${samples_id}
 do
-    docker run -v $(pwd):$(pwd) -v ${FILE_PATH}:${FILE_PATH} -v ${ALIGNMENT_PATH}:${ALIGNMENT_PATH} -w $(pwd) xiang2019/samtools:v1.9 samtools view -@ 8 -q 255 -b -h ${ALIGNMENT_PATH}/${f}.bamAligned.sortedByCoord.out.bam > ${ALIGNMENT_PATH}/${f}.unique.bam
+    docker run -v $(pwd):$(pwd) -v ${FILE_PATH}:${FILE_PATH} -v ${ALIGNMENT_PATH}:${ALIGNMENT_PATH} -w $(pwd) xiang2019/samtools:v1.9 samtools view -@ 8 -q 255 -b -h -o ${ALIGNMENT_PATH}/${f}.unique.bam ${ALIGNMENT_PATH}/${f}.bamAligned.sortedByCoord.out.bam ${CHROMOSOMES_KEPT}
 done
+
+
