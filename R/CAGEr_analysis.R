34c327a01c89d33071d99546c7cf21537af6a788
yexiang2046 <8704644+yexiang2046@users.noreply.github.com>
2024-06-25 20:35:24 +0000

    CAGEr scripts

--- R/CAGEr_analysis.R
+++ R/CAGEr_analysis.R
@@ -20,7 +20,7 @@ args <- commandArgs(trailingOnly = TRUE)
 
 # Read the input file
 input_file <- args[1]
-output_file <- args[2]
+output_label <- args[2]
 
 input_data <- read.table(input_file, header = TRUE)
 bam_files <- input_data[,2]
@@ -28,5 +28,30 @@ bam_files <- input_data[,2]
 # Create CAGEexp object
 cage_data <- CAGEexp(inputFiles = bam_files, inputFilesType = "bamPairedEnd" genome = BSgenome.Hsapiens.UCSC.hg38, nthreads = 24)
 
+# Set sequencing protocol to RAMPAGE
+seqProt(cage_data) <- "RAMPAGE"
+
+# Specify the genome if not set during initialization
+genome(cage_data) <- "BSgenome.Hsapiens.UCSC.hg38.KSHV" # adjust as needed
+
+# Quantify CAGE tags to identify TSS
+getCTSS(cage_data)
+quantifyCTSS(cage_data)
+
+# Normalize tag counts
+normalizeTagCount(cage_data, method = "quantile") # You can choose other normalization methods
+
+# Cluster CTSS to define consensus clusters (i.e., TSS regions)
+clusterCTSS(cage_data, threshold = 100) # The threshold can be adjusted
+
+# Aggregate TSSs into tag clusters
+aggregateTagClusters(cage_data)
+
+# Export results or perform additional analysis
+tagClusters <- getTagClusters(cage_data)
+
+# Optionally, output the tag clusters to a file
+write.table(tagClusters, file = paste0(output_label,"_tagClusters.txt"), sep = "\t", quote = FALSE)
+saveRDS(cage_data, file = paste0(output_label,"_cage_data.rds")
 
 
